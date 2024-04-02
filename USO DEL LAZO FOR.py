8253323fcdaa45c120294e55de06edbbbe9e1da0
tano1963 <guerrid@gmail.com>
2024-04-02 17:22:24 -0300

    Este es el septimo commit de pruebas

---
+++ USO DEL LAZO FOR.py
@@ -0,0 +1,24 @@
+#Aqui itero sobre una lista
+numeros =[1,2,3,4,5,6]
+for numero in numeros :
+    print (numero)
+
+
+print("")
+
+
+#Aqui creo que hago lo que estoy acostumbrado a hacer
+for i in range(10):
+    print("Hello Kity")
+    
+print("")  
+
+#un For Mezclado con un else 
+for i in range(10):
+        print("Hello DAN")
+else:
+        print("Termino el bucle")
+
+
+
+
--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 print("Hello Daniel   SOLOOOOOOOOOOO SIEMPREeeeeeeeessssssssss")
+numeros =[1,2,3,4,5,6]
+for numero in numeros :
+    print (numero)
+    
+
+
 
 
