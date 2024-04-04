dc04a41590b9fbc4b678de21bd23787c2a44c037
tano1963 <guerrid@gmail.com>
2024-04-04 13:29:34 -0300

    Este es el octavo commit

--- clases.py
+++ clases.py
@@ -12,25 +12,44 @@ class Persona:
     def __init__(self,nombre, apellido): #Este es el contructor de la clase
         self.nombre = nombre
         self.apellido = apellido
-    
-my_person = Persona("danie", "guerri")
 
-print(f"{my_person.nombre} {my_person.apellido}")
+Persona = Persona ("","")    
+
+
+#Esta es la creacion de la instancia de la clase a travez
+#de esa instancia es que se acceden a los atributos de la clase y a los procedimientos de ella
+
+        
+print ("Ingresa el nombre----")
+Persona.nombre = input ("")
+print ("Ingresa el apellido----")
+Persona.apellido = input ("")
+
+nombre = Persona.nombre
+apellido = Persona.apellido
+
+print(nombre)
+print (apellido)
+
+
+#my_person = Persona(nomb.nombre, apell.apellido)
+
+#print(f"{my_person.nombre} {my_person.apellido}")
 
 #veamos funciones definidas dentro de la clase
 
-class Persona:
-    def __init__(self,nombre, apellido): 
-        self.full_name = ("daniel" "guerri" )
-    def caminar (self):
-        print (f"{self.full_name} Esta Caminando") #Esta funcion pertenece a la clase y uso variables definidas dentro de la clase
+#class Persona:
+#    def __init__(self,nombre, apellido): 
+#        self.full_name = ("daniel" "guerri" )
+#    def caminar (self):
+#        print (f"{self.full_name} Esta Caminando") #Esta funcion pertenece a la clase y uso variables definidas dentro de la clase
         
-mi_persona = Persona("daniel", "guerri")
-print(mi_persona.caminar)
+#mi_persona = Persona("daniel", "guerri")
+#print(mi_persona.caminar)
    
-mi_persona.caminar()  
-mi_persona.full_name = "PePe"   
-print (mi_persona.full_name)
+#mi_persona.caminar()  
+#mi_persona.full_name = "PePe"   
+#print (mi_persona.full_name)
 
 
         
